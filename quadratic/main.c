2986f6516b13805d9dfdffcd7891bfce0a3d776a
matlire <matmikhvlad@gmail.com>
2025-08-22 22:36:53 +0300

    Added comments

--- quadratic/main.c
+++ quadratic/main.c
@@ -1,14 +1,18 @@
 #include "quadratic/quadratic.h"
 
-int main() {    
+int main() {
+    // Create equation struct
     eq_t eq;
     init_eq(&eq);
     
+    // Parse user input
     bool parse_result = io_parse_input(&eq);
     if (!parse_result) return 0;
 
+    // Calculate root
     uint8_t root_number = calc_roots(&eq);
-
+    
+    // Print formatted result
     io_print_output(&eq);
 
     return 0;
